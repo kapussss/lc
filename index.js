d8ce50cdd4b8ec25a3551a95bb225f9f9fd4cb8c
kapussss <ktan070269@gmail.com>
2026-05-09 14:51:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 console.log('🚀 LC79 API - KHỞI ĐỘNG...');
 
